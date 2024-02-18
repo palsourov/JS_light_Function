3555add3181d92304be703f57e9cf61182b0d403
palsourov <palsourov365@gmail.com>
2024-02-18 13:25:29 +0600

    light on of funciton create using javascript

---
+++ script.js
@@ -0,0 +1,8 @@
+function lightOn(){
+    let lightON=document.querySelector(".lgtOff");
+    lightON.setAttribute("src","./Img/lightON.png");
+}
+function  lightOff(){
+    let lightOFF=document.querySelector(".lgtOff");
+       lightOFF.setAttribute("src","./Img/lightOff.png");
+}
